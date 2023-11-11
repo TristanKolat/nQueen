d6bd693be513f06a8dfa771c6bb8f3f5111e2179
Tristan Thomas Kolat <tkolat@tristans-air.mynetworksettings.com>
2023-11-11 02:03:42 -0500

    added place queen command

---
+++ devtools/Commands/Queen_Command/Place_Queen_Command.cpp
@@ -0,0 +1,15 @@
+// PlaceQueenCommand.cpp
+
+#include "Place_Queen_Command.h"
+
+Place_Queen_Command::Place_Queen_Command(Array_Iterator<size_t> &iter, size_t row)
+    : iter_(iter), 
+      row_(row) 
+{}
+
+void Place_Queen_Command::execute(void) {
+    // The iterator should already be at the correct column.
+    if (!iter_.is_done()) {
+        *iter_ = row_; // Place the queen at the current iterator position, which is the correct column.
+    }
+}
---
+++ devtools/Commands/Queen_Command/Place_Queen_Command.h
@@ -0,0 +1,32 @@
+// PlaceQueenCommand.h
+/**
+ * @author Tristan Kolat
+ * 
+ * @date   Nov 11th, 2023
+ * @class A command to place a queen on the chessboard for the N-Queens problem.
+*/
+
+#ifndef _PLACE_QUEEN_COMMAND_H_
+#define _PLACE_QUEEN_COMMAND_H_
+
+#include "../Interface/Expr_Command.h"
+#include "../../Array/Array.h"
+#include "../../Iterator/iterArray/Array_Iterator.h"
+
+
+class Place_Queen_Command : public Expr_Command {
+public:
+    // Construct an reference to the Board(Array) and row number
+    Place_Queen_Command(Array_Iterator<size_t> &iter, size_t row);
+    virtual ~Place_Queen_Command() { }
+    //execute placing the queen onto the board array
+    virtual void execute(void);
+
+private:
+    //referenced column from the iterator in the chess board array for assigning a row number 
+    Array_Iterator<size_t> &iter_;
+    //row number to place queen
+    size_t row_;
+};
+
+#endif // !_PLACE_QUEEN_COMMAND_H_
