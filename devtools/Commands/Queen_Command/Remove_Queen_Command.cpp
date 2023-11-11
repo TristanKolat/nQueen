54b85dc3e783923f94b70a39cd3921960d12c4ab
Tristan Thomas Kolat <tkolat@tristans-air.mynetworksettings.com>
2023-11-11 02:29:16 -0500

    added remove queen command

---
+++ devtools/Commands/Queen_Command/Remove_Queen_Command.cpp
@@ -0,0 +1,21 @@
+// Remove_Queen_Command.h
+/**
+ * @author Tristan Kolat
+ * 
+ * @date   Nov 11th, 2023
+ * @class A command to remove a queen from the chessboard for the N-Queens problem.
+ */
+
+
+#include "Remove_Queen_Command.h"
+
+Remove_Queen_Command::Remove_Queen_Command(Array_Iterator<int> &iter)
+    : iter_(iter) 
+{}
+
+void Remove_Queen_Command::execute(void) {
+    // The iterator should already be at the correct column.
+    if (!iter_.is_done()) {
+        *iter_ = -1; // Remove the queen by setting the column to -1, indicating no queen is placed.
+    }
+}
---
+++ devtools/Commands/Queen_Command/Remove_Queen_Command.h
@@ -0,0 +1,29 @@
+// Remove_Queen_Command.h
+/**
+ * @author Tristan Kolat
+ * 
+ * @date   Nov 11th, 2023
+ * @class A command to remove a queen from the chessboard for the N-Queens problem.
+ */
+
+#ifndef _REMOVE_QUEEN_COMMAND_H_
+#define _REMOVE_QUEEN_COMMAND_H_
+
+#include "../Interface/Expr_Command.h"
+#include "../../Array/Array.h"
+#include "../../Iterator/iterArray/Array_Iterator.h"
+
+class Remove_Queen_Command : public Expr_Command {
+public:
+    // Construct a reference to the Board(Array) iterator
+    Remove_Queen_Command(Array_Iterator<int> &iter);
+    virtual ~Remove_Queen_Command() { }
+    // Execute removal of the queen from the board array
+    virtual void execute(void) override;
+
+private:
+    // Referenced iterator in the chess board array to remove a queen from
+    Array_Iterator<int> &iter_;
+};
+
+#endif // !_REMOVE_QUEEN_COMMAND_H_
