531276cd81c9c3e7afa341d89eded236cea53e52
Tristan Thomas Kolat <tkolat@tristans-air.mynetworksettings.com>
2023-11-11 19:40:02 -0500

    added concrete command factory

---
+++ devtools/Factory/Cmd_Factory/Concrete_Expr_Command_Factory.h
@@ -0,0 +1,28 @@
+/**
+ * @file Concrete_Expr_Command_Factory.h
+ * @author Tristan Kolat
+ * 
+ * @date Nov 11th, 2023
+ * 
+ * @brief Concrete factory for creating command objects related to placing and removing queens in the N-Queens problem.
+ */
+
+#ifndef _CONCRETE_EXPR_COMMAND_FACTORY_H_
+#define _CONCRETE_EXPR_COMMAND_FACTORY_H_
+
+#include "Interface/Expr_Command_Factory.h"
+#include "../../Iterator/iterArray/Array_Iterator.h"
+
+/**
+ * @class Concrete_Expr_Command_Factory
+ * @brief Concrete factory for creating command objects.
+ *
+ * Implements Expr_Command_Factory to provide concrete command objects for the N-Queens problem.
+ */
+class Concrete_Expr_Command_Factory : public Expr_Command_Factory {
+public:
+    virtual Place_Queen_Command* create_place_queen_command(Array<int> &board, int row);
+    virtual Remove_Queen_Command* create_remove_queen_command(Array<int> &board, int row);
+};
+
+#endif // _CONCRETE_EXPR_COMMAND_FACTORY_H_
