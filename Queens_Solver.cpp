e4a90311bd8a7dd0c1f866df1337afcae7d7847e
tkolat <tkolat@NZXT.localdomain>
2023-11-13 18:31:14 -0500

    added cpp outline

---
+++ Queens_Solver.cpp
@@ -0,0 +1,25 @@
+// Queens_Solver.cpp
+
+#include "Queens_Solver.h"
+
+Queens_Solver::Queens_Solver(Stack<Array<int>>& solutions)
+: solutions(solutions),
+  factory() 
+{}
+
+void Queens_Solver::solve() {
+
+}
+
+void Queens_Solver::solve_placements(Array<int>& board, int col) {
+
+}
+
+bool Queens_Solver::is_Safe(Array<int>& board, int col ,int row) {
+
+}
+
+Stack<Array<int>>& Queens_Solver::get_solutions() const {
+
+}
+
