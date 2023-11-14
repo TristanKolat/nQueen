5d441e7b8ff2364be6562ccf586dfb0edb24eeb8
tkolat <tkolat@NZXT.localdomain>
2023-11-14 00:46:33 -0500

    added commented pseudo-code

--- driver.cpp
+++ driver.cpp
@@ -6,5 +6,33 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
+    //initalize solution stack
+    //initialize other variables to be used
 
+    //start loop for user interaction
+        //ask user for number of queens and store input
+        //if user inputs QUIT then break loop
+
+        //pass user integer value and solutions stack into queens solver
+        //start recursion with solve()
+
+        //display to user how many solutions were found
+
+        //make do while loop
+        //in do while as user what solution they want to see. 
+        //keep looping so that user can view different solutions
+
+        //add another check in case user wants to quit
+
+        //add another check to see if user wants to see all solutions in one display
+
+        //else retrieve the specific solution 
+            //store into temp array
+            //display that solution held in the temp array
+
+        //end do while loop
+
+        //clear solutions stack in case user wants to try another solution
+
+    return 0;
 }
