e61dcc63f3b090493784cc2ec4a02a03148b2a9a
tkolat <tkolat@NZXT.localdomain>
2023-11-14 00:32:02 -0500

    started driver

---
+++ driver.cpp
@@ -0,0 +1,10 @@
+#include "Queens_Solver.h"
+#include "devtools/Array/Array.h"
+#include "devtools/Stack/Stack.h"
+#include "Array_Iterator.h"
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+
+}
